fabb215d6383407fa0837fe30372012e64d59a90
Imanuel Febie <imanuel.febie@binus.ac.id>
2020-01-14 18:13:47 +0700

    slight changes

--- manage_me/projects/routes.py
+++ manage_me/projects/routes.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Blueprint, render_template, redirect, url_for
 from manage_me.projects.forms import ProjectCreateForm, MilestoneCreateForm
 
@@ -6,13 +7,18 @@ projects = Blueprint('projects', __name__)
 @projects.route('/home')
 def home():
     '''Homescreen with projects overview'''
-    return render_template('home.html')
+    date = datetime.date.today()
+    print(date)
+    return render_template('home.html', date=date)
 
 @projects.route('/projects/new')
 def project_create():
     '''Create new project'''
     form = ProjectCreateForm()
     return render_template('project_create.html', form=form)
+
+
+
 
 @projects.route('/milestone/create')
 def milestone_create():
